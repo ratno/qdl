de73af6d06fce17a16796707d6d4893a43da756d
ratno putro sulistiyono <ratno@knoqdown.com>
2018-01-15 18:29:07 +0700

    add route service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace QDL;
+
+
+class ServiceProvider extends Illuminate\Support\ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
