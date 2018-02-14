2adeb6002c49419e1a5af243bcd0f8161be4750e
ratno putro sulistiyono <ratno@knoqdown.com>
2018-02-14 14:16:07 +0700

    helpers.php: tambahkan fungsi from_camel_case

---
+++ src/helpers.php
@@ -0,0 +1,10 @@
+<?php
+if(!function_exists('from_camel_case')) {
+    function from_camel_case($str) {
+        $str[0] = strtolower($str[0]);
+        $func = function($c) {
+            return "_" . strtolower($c[1]);
+        };
+        return preg_replace_callback('/([A-Z])/', $func, $str);
+    }
+}
